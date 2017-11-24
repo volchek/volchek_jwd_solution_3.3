740635f28a83780ff68f6216a9dafafc78c6eab7
Oksana Volchek <VolchekOA@gmail.com>
2017-11-24 16:28:25 +0200

    Replaced some whitespaces with tabs

--- jwd_task_3.3/src/main/java/by/tr/web/controller/command/impl/Parse.java
+++ jwd_task_3.3/src/main/java/by/tr/web/controller/command/impl/Parse.java
@@ -19,7 +19,7 @@ import by.tr.web.service.exception.ServiceException;
 
 public class Parse implements ControllerCommand {
 	
-    private static final String PARSER_TYPE = "parser";
+	private static final String PARSER_TYPE = "parser";
     
     
 	@Override
@@ -31,28 +31,26 @@ public class Parse implements ControllerCommand {
 		String parser = request.getParameter(PARSER_TYPE);
 
 		RequestDispatcher d = null;
-			
+		
 		try {
 			List<CD> result = xmlObject.getCD(parser);
 			
 			ResultHolder.getInstance().setResult(result);
 			
-			if (result != null && !result.isEmpty()){				
+			if (result != null && !result.isEmpty()){
 				request.setAttribute(CommandConsts.ATTR_NAME_TAG, result.subList(CommandConsts.START_INDEX, CommandConsts.RECORD_COUNT));
 				request.setAttribute(CommandConsts.PAGE_COUNT_TAG, result.size() / CommandConsts.RECORD_COUNT);
-			    request.setAttribute(CommandConsts.CURRENT_PAGE_TAG, CommandConsts.START_INDEX);
+				request.setAttribute(CommandConsts.CURRENT_PAGE_TAG, CommandConsts.START_INDEX);
 				d = request.getRequestDispatcher(CommandConsts.RESULT_PAGE);
 			}
-			else { throw new ControllerException(); }
+			else { d = request.getRequestDispatcher(CommandConsts.CONTENT_ERROR_PAGE); }
 		}
 		catch(ServiceException ex){
 			d = request.getRequestDispatcher(CommandConsts.PARSER_ERROR_PAGE);
-		}
-		catch(ControllerException ex){
-			d = request.getRequestDispatcher(CommandConsts.CONTENT_ERROR_PAGE);
 		}
 		
 		d.forward(request, response);
 		
 	}
 }
+
--- jwd_task_3.3/src/main/java/by/tr/web/controller/command/impl/Show.java
+++ jwd_task_3.3/src/main/java/by/tr/web/controller/command/impl/Show.java
@@ -16,25 +16,25 @@ import by.tr.web.entity.CD;
 public class Show implements ControllerCommand {
    
 	private List<CD> currentCD = null;
-    private static final String PAGE_Param = "page";	
+	private static final String PAGE_Param = "page";	
 	
 
-    @Override
+	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{ 
 
-    	List<CD> result = ResultHolder.getInstance().getResult();
+		List<CD> result = ResultHolder.getInstance().getResult();
   
-    	RequestDispatcher d = null;
+		RequestDispatcher d = null;
 		int currentPage = CommandConsts.START_INDEX + 1;
 				
 		String pageParamValue = request.getParameter(PAGE_Param);
 
-        if (pageParamValue != null) {
-           currentPage = Integer.parseInt(pageParamValue) - 1;
-        }
+		if (pageParamValue != null) {
+			currentPage = Integer.parseInt(pageParamValue) - 1;
+		}
         
-        getCDSubarray(currentPage, result);
-        setAttributes(request, currentPage, result);
+		getCDSubarray(currentPage, result);
+		setAttributes(request, currentPage, result);
 
 		d = request.getRequestDispatcher(CommandConsts.RESULT_PAGE);
 		d.forward(request, response);
@@ -42,20 +42,20 @@ public class Show implements ControllerCommand {
 	}
     
     
-    private void setAttributes(HttpServletRequest request, int currentPage, List<CD> result){
-        request.setAttribute(CommandConsts.ATTR_NAME_TAG, currentCD);
-        request.setAttribute(CommandConsts.CURRENT_PAGE_TAG, currentPage + 1);
-        request.setAttribute(CommandConsts.PAGE_COUNT_TAG, result.size() / CommandConsts.RECORD_COUNT);
-    }
+		private void setAttributes(HttpServletRequest request, int currentPage, List<CD> result){
+		request.setAttribute(CommandConsts.ATTR_NAME_TAG, currentCD);
+		request.setAttribute(CommandConsts.CURRENT_PAGE_TAG, currentPage + 1);
+		request.setAttribute(CommandConsts.PAGE_COUNT_TAG, result.size() / CommandConsts.RECORD_COUNT);
+	}
     
     
-    private void getCDSubarray(int currentPage, List<CD> result){
-        if ( (currentPage * CommandConsts.RECORD_COUNT + CommandConsts.RECORD_COUNT) < result.size() ){
-	        currentCD = result.subList(currentPage * CommandConsts.RECORD_COUNT, currentPage * CommandConsts.RECORD_COUNT + CommandConsts.RECORD_COUNT);
-        }
-        else {
-	        currentCD = result.subList(currentPage * CommandConsts.RECORD_COUNT, result.size());
-        }
-    
-    }
+	private void getCDSubarray(int currentPage, List<CD> result){
+		if ( (currentPage * CommandConsts.RECORD_COUNT + CommandConsts.RECORD_COUNT) < result.size() ){
+			currentCD = result.subList(currentPage * CommandConsts.RECORD_COUNT, currentPage * CommandConsts.RECORD_COUNT + CommandConsts.RECORD_COUNT);
+		}
+		else {
+			currentCD = result.subList(currentPage * CommandConsts.RECORD_COUNT, result.size());
+		}
+	}
+	
 }
